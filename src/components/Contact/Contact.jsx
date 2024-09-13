76d45ccb93b66ad80032edc07d051976657ffd01
Olena-Synytsia <alenasinitsa85@gmail.com>
2024-09-13 19:05:11 +0300

    ContactList start

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,7 +1,15 @@
 import s from "./Contact.module.css";
 
-const Contact = () => {
-  return <div className={s.container}>Contact</div>;
+const Contact = ({ id, name, number, handleDeleteContact }) => {
+  return (
+    <div className={s.container}>
+      <p className={s.name}>{name}</p>
+      <p className={s.number}>{number}</p>
+      <button onClick={() => handleDeleteContact(id)} className="btn border">
+        Delete
+      </button>
+    </div>
+  );
 };
 
 export default Contact;
--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,12 +1,30 @@
+import { useState } from "react";
 import Contact from "../Contact/Contact";
+import contactsData from "../../UserData/contacts.json";
 import s from "./ContactList.module.css";
 
 const ContactList = () => {
-    return (
-        <div>
-            <p className={s.container}>ContactList</p>
-      <Contact/>
-      </div>
+  const [contacts, setContacts] = useState(contactsData);
+
+  const handleDeleteContact = (id) => {
+    setContacts((prev) => prev.filter((item) => item.id !== id));
+  };
+
+  return (
+    <div className={s.container}>
+      <ul className={s.element}>
+        {contacts.map((contact) => (
+          <li className={s.item} key={contact.id}>
+            <Contact
+              name={contact.name}
+              number={contact.number}
+              id={contact.id}
+              onDelete={() => handleDeleteContact(contact.id)}
+            />
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 };
 

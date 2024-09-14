f8ab931a72a28a9ac34da046e3357e4695a5ef0e
Olena-Synytsia <alenasinitsa85@gmail.com>
2024-09-14 19:57:28 +0300

    SearchBox Start

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,7 +1,22 @@
 import s from "./SearchBox.module.css";
 
-const SearchBox = () => {
-  return <div className={s.container}>SearchBox</div>;
+const SearchBox = ({ searchValue, onSearchChange }) => {
+  const handleChange = (event) => {
+    onSearchChange(event.target.value);
+  };
+  return (
+    <div className={s.container}>
+      <label htmlFor="searchInput">
+        <span>Find contacts by name</span>
+        <input
+          type="text"
+          id="searchInput"
+          value={searchValue}
+          onChange={handleChange}
+        />
+      </label>
+    </div>
+  );
 };
 
 export default SearchBox;

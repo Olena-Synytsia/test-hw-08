d89a057339fc923050f257594af3f5eaff9ee0b0
Olena-Synytsia <alenasinitsa85@gmail.com>
2024-09-15 14:36:36 +0300

    ContactForm css

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -8,14 +8,35 @@ const ContactForm = () => {
     number: "",
   };
 
-  const handleSubmit = () => console.log("SRART");
+  const handleSubmit = (values, options) => {
+    console.log("SRART");
+    console.log(values);
+    options.resetForm();
+  };
   return (
     <div className={s.container}>
       <Formik initialValues={initialValues} onSubmit={handleSubmit}>
-        <Form>
-          <Field name="name"></Field>
-          <Field name="number"></Field>
-          <button type="submit">Add contact</button>
+        <Form className={s.form}>
+          <label className={s.label}>
+            <span className={s.span}>Name</span>
+            <Field
+              name="name"
+              className={s.input}
+              placeholder="Enter your name"
+            ></Field>
+          </label>
+          <label className={s.label}>
+            <span className={s.span}>Number</span>
+            <Field
+              name="number"
+              className={s.input}
+              placeholder="Enter your phone number"
+            ></Field>
+          </label>
+
+          <button type="submit" className={s.btn}>
+            Add contact
+          </button>
         </Form>
       </Formik>
     </div>

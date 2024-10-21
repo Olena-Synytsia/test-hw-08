a96b5720f4daa24bbfe38f4140fe83c5b9ec37e2
Olena-Synytsia <alenasinitsa85@gmail.com>
2024-10-21 10:15:15 +0300

    motion

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,14 +1,25 @@
+import { motion } from "framer-motion";
+import { slideInFromBot, slideInFromTop } from "../../motion/motion.js";
+
 import s from "./HomePage.module.css";
 
 const HomePage = () => {
   return (
     <div className={s.container}>
-      <h2 className={s.text}>
+      <motion.h2
+        initial="hidden"
+        animate="visible"
+        variants={slideInFromTop(1)}
+        className={s.text}
+      >
         Phone Book is a modern application that will help you easily organize
         and store all your contacts in one place. Thanks to the intuitive
         interface, you can quickly add, edit and delete contacts.
-      </h2>
-      <img
+      </motion.h2>
+      <motion.img
+        initial="hidden"
+        animate="visible"
+        variants={slideInFromBot(1)}
         className={s.img_home}
         src="https://cdn.pixabay.com/photo/2014/04/03/10/28/telephone-310544_1280.png"
         alt="phone"
